61bca9ac87f8eeb344a45d67e1c8fa1f0b9b379f
Jonas Kapitzke <mail@jkapitzke.de>
2018-06-11 15:35:55 +0200

    changed default config, added some more or less useless configurations setups, added data of some runs.

--- src/main/java/mas_project/ExperimentSetup.java
+++ src/main/java/mas_project/ExperimentSetup.java
@@ -19,12 +19,12 @@ public class ExperimentSetup {
 
 	public ExperimentSetup(int exp) {
 		// default values
-		runTimeInterval = ints(1000 * 1000);
+		runTimeInterval = ints(1000 * 10000);
 		commRangeInterval = ints(1, 1000 * 1000);
 		commReliabilityInterval = doubles(1.0);
 		numTaxisInterval = ints(5);
 		numCustomersInterval = ints(10);
-		newCustomerProbInterval = doubles(0.01);
+		newCustomerProbInterval = doubles(0.005);
 		seeRangeInterval = ints(1000);
 		lazyProbInterval = doubles(0.0);
 		filename = String.valueOf(exp) + "_runtime" + runTimeInterval.get(0);
@@ -46,8 +46,54 @@ public class ExperimentSetup {
 		case 5:
 			numTaxisInterval = ints(2, 5, 10, 20);
 			break;
+		case 6:
+			numTaxisInterval = ints(2, 5, 10, 20);
+			newCustomerProbInterval = doubles(0.001, 0.005, 0.02, 0.1);
+			break;
+		case 7:
+			numTaxisInterval = ints(50);
+			numCustomersInterval = ints(10);
+			newCustomerProbInterval = doubles(0.2);
+			seeRangeInterval = ints(1000);
+			commRangeInterval = ints(10 * 1000);
+			break;
+		case 8:
+			numTaxisInterval = ints(2, 5, 10);
+			seeRangeInterval = ints(10, 100, 1000, 10 * 1000, 100 * 1000, 1000 * 1000);
+			break;
+		case 9:
+			seeRangeInterval = ints(10, 31, 100, 310, 1000, 3100, 10 * 1000, 31 * 1000, 100 * 1000, 310 * 1000,
+					1000 * 1000);
+			break;
+		case 10:
+			// if seeRange is big, then impact of commRange is also big - as can be seen in
+			// config 3.
+			// This is to find parameters where impact of comm is big. We already saw
+			// increasing seeRange brings good impact. Let's play around more:
+			seeRangeInterval = ints(100 * 1000);
+			numTaxisInterval = ints(2, 5, 10);
+			newCustomerProbInterval = doubles(0.001, 0.005);
+			break;
+		case 11:
+			// if seeRange is big, then impact of commRange is also big - as can be seen in
+			// config 3.
+			// This is to find parameters where impact of comm is big. We already saw
+			// increasing seeRange brings good impact. Let's play around more:
+			seeRangeInterval = ints(100 * 1000);
+			numTaxisInterval = ints(10);
+			newCustomerProbInterval = doubles(0.005);
+			break;
+		case 12: // DOES NOT WORK - stops after like 26 iterations
+			// makes config 10 together with 11
+			seeRangeInterval = ints(100 * 1000);
+			numTaxisInterval = ints(2, 5, 10);
+			newCustomerProbInterval = doubles(0.01);
+			break;
+
 		default:
 			throw new IllegalArgumentException("Experiment not defined.");
 		}
+
+		filename = String.valueOf(exp) + "_runtime" + runTimeInterval.get(0);
 	}
 }
